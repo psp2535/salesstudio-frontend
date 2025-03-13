dd82ab60ecf42d20910b8d4f14c29a2196e08037
psp2535 <saipranav974@gmail.com>
2025-03-14 03:40:12 +0530

    backend

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./style.css";
 import companyLogo from "./assets/image.png"; // Ensure the image is in src/assets/
 
-const API_URL = "https://salesstudio-frontend.vercel.app/"; // ✅ Local backend URL
+const API_URL = "https://salesstudio-backend-production.up.railway.app/"; // ✅ Local backend URL
 
 function App() {
   const [coupon, setCoupon] = useState(null);
